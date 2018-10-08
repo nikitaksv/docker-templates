686be75e2dcd669757da1db1c170b8096782b988
YekitKsv <nickitakrac@gmail.com>
2018-10-08 14:00:27 +0300

    added redis for yii2

--- yii2/src/common/config/main.php
+++ yii2/src/common/config/main.php
@@ -7,7 +7,12 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\redis\Cache',
+            'redis' => [
+                'hostname' => 'redis',
+                'port' => 6379,
+                'database' => 0,
+            ]
         ],
-    ],
+    ],   
 ];
